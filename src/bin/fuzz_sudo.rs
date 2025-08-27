a700ab1ae92c94d0fa75025861959a8a3f55ccac
jegher <andrea.jegher@pm.me>
2025-08-27 15:44:22 +0200

    Minor changes

--- src/bin/fuzz_sudo.rs
+++ src/bin/fuzz_sudo.rs
@@ -7,23 +7,26 @@ const BASE_PATH: &'static str = "/var/run/sudo-rs/ts";
 
 fn main() {
     fuzz!(|data: &[u8]| {
-        let path = "/root/additional";
-        if let Ok(mut output) = File::create(path) {
-            if let Ok(s) = std::str::from_utf8(data) {
+        if let Ok(s) = std::str::from_utf8(data) {
+            let path = "/root/additional";
+
+            if let Ok(mut output) = File::create(path) {
                 match write!(output, "{}", s) {
                     Ok(_) => {
                         sudo_rs::sudo_main();
-                        unsafe {
-                            let uid = libc::getuid();
-                
-                            // a succeful login will create a session file
-                            // that might create false positives
-                            let mut path = PathBuf::from(BASE_PATH);
-                            path.push(uid.to_string());
-                            let _ = std::fs::remove_file(path);
 
-                            panic!("User passed after sudo -l {}", uid);
+                        let uid;
+                        unsafe {
+                            uid = libc::getuid();
                         }
+
+                        // a succeful login will create a session file
+                        // that might create false positives
+                        let mut path = PathBuf::from(BASE_PATH);
+                        path.push(uid.to_string());
+                        let _ = std::fs::remove_file(path);
+
+                        panic!("User passed after sudo -l {}", uid);
                     }
                     Err(_) => {
                         return;
